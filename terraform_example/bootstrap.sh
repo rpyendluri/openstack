3f30fb42c1c81dc0f2361b8f63730cd7f42d209a
Pradeep Buditi <pbuditi@palo-it.com>
2017-05-18 13:23:39 +0800

    adding ufw

--- terraform_example/bootstrap.sh
+++ terraform_example/bootstrap.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo "Terraform Lauch was successful" >> /etc/motd
+apt-get update && sudo apt-get upgrade
+apt-get install -y apache2
+systemctl enable apache2
 
+cat <<EOF > /var/www/html/index.html
+<html>
+<body>
+<p>hostname is: $(hostname)</p>
+</body>
+</html>
+EOF
+chown -R apache:apache /var/www/html
+systemctl restart apache2
+ufw allow in "Apache Full"
